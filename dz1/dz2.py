76637a094bfcf42b87d5dbcc84ed3f8bb2d213e2
Инесса-Жарких <79988379+inessa-zharkikh@users.noreply.github.com>
2023-09-10 20:52:54 +0300

    Add files via upload

---
+++ dz1/dz2.py
@@ -0,0 +1,10 @@
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+f = float(input())
+if f - d != 0:
+    k = (a * b - c) / (f - d)
+    print(k)
+else:
+    print('Делить на ноль нельзя!')
---
+++ dz1/dz3.py
@@ -0,0 +1,5 @@
+x = float(input())
+y = float(input())
+z = float(input())
+k = (x ** 2 + y ** 2 + z ** 2) ** 0.5
+print(k)
